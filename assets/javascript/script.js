4e87d04a3fe7a68ed3675d825f4dc71ca8bee415
Sajjad <bkhshndhs898@gmail.com>
2021-11-16 19:11:41 +0330

    bug fixing

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,8 @@
 //? HTML elements
 const searchButton = document.getElementById("btnSearch");
-const btnDownload = document.getElementsByClassName("btnDownload")[0];
+const btnDownload1080 = document.getElementsByClassName("btnDownload")[0];
+const btnDownload720 = document.getElementsByClassName("btnDownload")[1];
+const btnDownload680 = document.getElementsByClassName("btnDownload")[2];
 const selectBox = document.getElementById("selectBox");
 const searchInput = document.getElementById("inputSearch");
 const resultMedia = document.getElementsByClassName("resultBoxMedia")[0];
@@ -11,6 +13,7 @@ const commentsNumber = document.querySelector(".comments .number");
 const userName = document.getElementById("userName");
 const Name = document.getElementById("Name");
 const profilePic = document.getElementById("profile");
+const loading = document.getElementsByClassName("loading")[0];
 
 //? vars
 const data = null;
@@ -27,6 +30,16 @@ searchButton.addEventListener("click", event => {
     const xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
 
+    //? on load start
+    xhr.onloadstart = function () {
+        loading.style.display = "block";
+    }
+
+    //? on load start
+    xhr.onloadend = function () {
+        loading.style.display = "none";
+    }
+
     if (inputVal.length != 0) {
 
         if (selectBoxVal == 1) {
@@ -39,12 +52,12 @@ searchButton.addEventListener("click", event => {
             let arr = inputVal.split("/");
             const ShortCode = arr[4];
 
-            console.log(ShortCode);
-
             //! open request
-            xhr.open("GET", `https://instagram47.p.rapidapi.com/post_details?shortcode=CWASiPpqguz`, true);
+            xhr.open("GET", `https://instagram47.p.rapidapi.com/post_details?shortcode=${ShortCode}`, true);
+
             xhr.setRequestHeader("x-rapidapi-host", "instagram47.p.rapidapi.com");
             xhr.setRequestHeader("x-rapidapi-key", "1312a34a16mshf6c97ff3206afe6p1602f7jsn357918cb1f08");
+            
             xhr.setRequestHeader("access-control-allow-origin", "*");
 
             //! onload
@@ -57,16 +70,17 @@ searchButton.addEventListener("click", event => {
                     const resUserName = response.body.owner.username;
                     const resName = response.body.owner.full_name;
                     const profilePicURL = response.body.owner.profile_pic_url
-                    const postURL = response.body.display_resources[0].src;
+                    const postURL1080 = response.body.display_resources[0].src;
+                    const postURL720 = response.body.display_resources[1].src;
+                    const postURL680 = response.body.display_resources[2].src;
                     const numberOfLikes = response.body.edge_media_preview_like.count;
                     const numberOfComments = response.body.edge_media_preview_comment.count;
-                    ////req 2
 
                     //! create img tag and set image src
                     const result = document.createElement("img");
                     result.style.width = "100%";
                     result.style.height = "100%";
-                    result.setAttribute("src", postURL);
+                    result.setAttribute("src", postURL680);
                     resultBox.appendChild(result);
 
 
@@ -82,9 +96,10 @@ searchButton.addEventListener("click", event => {
                     profilePic.setAttribute("src", profilePicURL);
 
 
-
                     //! set download btn href
-                    btnDownload.setAttribute("href", postURL);
+                    btnDownload1080.setAttribute("href", postURL1080);
+                    btnDownload720.setAttribute("href", postURL720);
+                    btnDownload680.setAttribute("href", postURL680);
 
                     resultMedia.style.opacity = "1";
                 }
@@ -95,6 +110,11 @@ searchButton.addEventListener("click", event => {
         }
 
     }
+
+        //? on erorr
+        xhr.onerror = function () {
+            loading.style.opacity = "none";
+        }
 
 
 
